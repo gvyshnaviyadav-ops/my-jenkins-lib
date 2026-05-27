634a5497ce93a2545f2b03fdab7ea33ece5cfa59
gvyshnaviyadav-ops <gvyshnaviyadav@cognam.com>
2026-05-27 16:58:04 +0530

    version

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -2,7 +2,7 @@ def call() {
     sh """
         echo "Building images..."
         
-        docker build -t todo-frontend ./todo-frontend
-        docker build -t todo-backend ./todo_project
+        docker build -t todo-frontend:${BUILD_NUMBER} ./todo-frontend
+        docker build -t todo-backend:${BUILD_NUMBER} ./todo_project
     """
 }
