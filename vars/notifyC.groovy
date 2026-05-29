c596c257752addcc956181c773d074646083a836
gvyshnaviyadav-ops <gvyshnaviyadav@cognam.com>
2026-05-29 18:40:37 +0530

    commit

--- vars/notifyC.groovy
+++ vars/notifyC.groovy
@@ -1,9 +1,9 @@
-def call( String credentialId,String jobName,String buildNumber,String buildUrl,String status,Boolean mentionAll = false) {
+def call( String credentialId,String jobName,String buildNumber,String buildUrl,String status,String mssg,Boolean mentionAll = true) {
 
     def prefix = mentionAll ? '<users/all> ' : ''
 
     def payload = """{
-        "text":"${prefix} *${jobName}* — Build *#${buildNumber}*\\n*Status:* ${status}\\n*Link:* ${buildUrl}"
+        "text":"${prefix} *${jobName}* — Build *#${buildNumber}*\\n*Status:* ${status}\\n*Message:* ${mssg}\\n*Link:* ${buildUrl}"
     }"""
 
     withCredentials([string(credentialsId: credentialId, variable: 'WEBHOOK_URL')]) {
