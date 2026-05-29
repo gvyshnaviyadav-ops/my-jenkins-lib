301d8ba8f4e8193fe193059868d11a54efb7ff82
gvyshnaviyadav-ops <gvyshnaviyadav@cognam.com>
2026-05-29 12:28:15 +0530

    commmit

---
+++ vars/fdeployApp.groovy
@@ -0,0 +1,11 @@
+def call(String stackName,String tag,String dockerHubUsername) {
+    sh """
+        echo "deploying ${stackName}"
+        export tag=${tag}
+        export duser=${dockerHubUsername}
+        sleep 20
+        docker stack deploy  -c docker-compose.yml ${stackName}"   
+        echo "Verify"
+        echo"docker stack ps ${stackName}"
+    """
+}
