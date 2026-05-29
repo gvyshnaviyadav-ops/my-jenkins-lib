adac46ec0e961a8eb4896df2a07fdf4f072b6ef4
gvyshnaviyadav-ops <gvyshnaviyadav@cognam.com>
2026-05-29 12:06:54 +0530

    commit

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -6,7 +6,7 @@ def call(String stackName,String tag,String dockerHubUsername) {
         echo "docker stack rm ${stackName} || true"
         sleep 20
         echo "docker stack deploy  -c docker-compose.yml ${stackName}"
-        docker compose -f test-compose.yml  up --build
+        docker compose up --build
             
         echo "Verify"
         echo"docker stack ps ${stackName}"
