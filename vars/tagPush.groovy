5aab869c804f9f79b335d61d11b2be13f5a9d272
gvyshnaviyadav-ops <gvyshnaviyadav@cognam.com>
2026-05-30 13:04:59 +0530

    comit

--- vars/tagPush.groovy
+++ vars/tagPush.groovy
@@ -4,5 +4,7 @@ def call(String tag,String dockerHubUsername) {
         echo "Tagging and pushing images to Docker Hub..."
         docker push ${dockerHubUsername}/todo-backend:${tag}
         docker push ${dockerHubUsername}/todo-frontend:${tag}
+        docker rmi ${dockerHubUsername}/todo-backend:${tag}
+        docker rmi ${dockerHubUsername}/todo-frontend:${tag}
     """
 }
